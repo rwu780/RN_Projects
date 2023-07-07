90ad7ff5c2d34cf9682ff463c73da926a55c67a7
James <rwu780@gmail.com>
2023-07-06 21:04:27 -0700

    Delete FavoritePlacesApp directory

--- FavoritePlacesApp/FavoritePlaces/components/places/PlaceForm.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function PlaceForm(): JSX.Element {
-    return (
-        <View>
-            <Text>The Place Form</Text>
-        </View>
-    )
-}
