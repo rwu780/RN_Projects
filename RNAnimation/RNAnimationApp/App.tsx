fc0b2155249d166391d4244ebb30c41f319a253c
James <rwu780@gmail.com>
2023-07-10 15:03:01 -0700

    update

--- RNAnimation/RNAnimationApp/App.tsx
+++ RNAnimation/RNAnimationApp/App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,22 +26,55 @@ import AnimatedSpring from './src/anim/AnimatedSpring';
 import AnimatedTiming from './src/anim/AnimatedTiming';
 import AnimatedValueXY from './src/anim/AnimatedValueXY';
 
+function AnimationList(): JSX.Element {
+  return (
+    <ScrollView style={{flex: 1}}>
+      <Transition />
+      <Rotation />
+      <Scale />
+      <Fade />
+      <AnimatedDecay />
+      <AnimatedSpring />
+      <AnimatedTiming />
+      <AnimatedValueXY />
+    </ScrollView>
+  );
+}
+
+function SideTabs(): JSX.Element {
+  return (
+    <View style={[styles.root, styles.tabScreenRoot]}>
+      <View style={styles.tabs}>
+        <Text style={styles.textTab}>Home</Text>
+      </View>
+      <View style={styles.tabs}>
+        <Text style={styles.textTab}>Info</Text>
+      </View>
+      <View style={styles.tabs}>
+        <Text style={styles.textTab}>Bio</Text>
+      </View>
+      <View style={styles.tabs}>
+        <Text style={styles.textTab}>Fab</Text>
+      </View>
+    </View>
+  );
+}
+
 function App(): JSX.Element {
+  const [showSideTab, setShowSideTab] = useState(true);
+
+  const switContent = () => {
+    setShowSideTab(!showSideTab);
+  };
+
   return (
     <SafeAreaView style={styles.root}>
-      <ScrollView
-        style={{flex: 1}}
-        >
-        <Transition />
-        <Rotation />
-        <Scale />
-        <Fade />
-        <AnimatedDecay />
-        <AnimatedSpring />
-        <AnimatedTiming />
-        <AnimatedValueXY />
-      </ScrollView>
-      
+      <Button
+        title={showSideTab ? 'Show Animation List' : 'Show Side Tabs'}
+        onPress={switContent}
+      />
+      {!showSideTab && <AnimationList />}
+      {showSideTab && <SideTabs />}
     </SafeAreaView>
   );
 }
@@ -50,6 +84,23 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  tabScreenRoot: {
+    justifyContent: 'center',
+  },
+  tabs: {
+    height: 60,
+    marginTop: 30,
+    borderRadius: 50,
+    backgroundColor: 'blue',
+    overflow: 'hidden',
+    alignContent: 'center',
+    justifyContent: 'center'
+    // marginStart: -50,
+  },
+  textTab: {
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  }
 });
 
 export default App;
