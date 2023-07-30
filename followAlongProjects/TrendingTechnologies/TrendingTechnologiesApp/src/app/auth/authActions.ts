eb577830786c27f7dc5cc0b20f96fd16ed188328
James <rwu780@gmail.com>
2023-07-29 22:06:38 -0700

    TrendingTechnologies::provide ui feedback

--- followAlongProjects/TrendingTechnologies/TrendingTechnologiesApp/src/app/auth/authActions.ts
+++ followAlongProjects/TrendingTechnologies/TrendingTechnologiesApp/src/app/auth/authActions.ts
@@ -1,97 +1,98 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios'
-import { URLS } from "../../constants/url";
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+import {URLS} from '../../constants/url';
 
-const registrationEndpoint = "uapi/user/registration"
-const loginEndpoint = "https://api.devio.org/uapi/user/login"
+const registrationEndpoint = 'uapi/user/registration';
+const loginEndpoint = 'https://api.devio.org/uapi/user/login';
 
 export const registerUser = createAsyncThunk(
-    'auth/register',
-    async initialPost => {
-        await setTimeout(() => {
-            console.log("Hello")
-        }, 100)
-        console.log("ABCDEEFF")
-        return {}
-        // await const response = 
-    }
-)
+  'auth/register',
+  async initialPost => {
+    await setTimeout(() => {
+      console.log('Hello');
+    }, 100);
+    console.log('ABCDEEFF');
+    return {};
+    // await const response =
+  },
+);
 
 export const login = createAsyncThunk(
-    'auth/login',
-    async (credential: LoginCredential, thunkApi) => {
-        
-        let formData = new FormData();
-        formData.append('userName', credential.userName)
-        formData.append('password', credential.password)
-        let response = 
-            await fetch(
-                buildParams(URLS.url + URLS.login.api, formData),
-                {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'content-type': 'multipart/form-data',
-                        ...URLS.headers
-                    }
-                }
-            )
-        let data = ((await response.json()) as ResponseType);
-        if (data.code !== 0) {
-            return thunkApi.rejectWithValue(data.msg);
-        }
-        return data
+  'auth/login',
+  async (credential: LoginCredential, thunkApi) => {
+    let formData = new FormData();
+    formData.append('userName', credential.userName);
+    formData.append('password', credential.password);
+    let response = handleData(
+      fetch(buildParams(URLS.url + URLS.login.api, formData), {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'content-type': 'multipart/form-data',
+          ...URLS.headers,
+        },
+      }),
+    );
+    let data = (await response) as ResponseType
+    if (data.code !== 0) {
+      return thunkApi.rejectWithValue(data.msg);
     }
-)
+    return data;
+  },
+);
 
 /**
  * 处理接口返回数据
- * @param doAction 
+ * @param doAction
  */
 function handleData(doAction: Promise<any>) {
-    return new Promise((resolve, reject) => {
-        doAction.then((res) => {
-            //解析Content-Type 防止将非json数据进行json转换
-            const type = res.headers.get('Content-Type');
-            if ((type || '').indexOf('json') !== -1) {
-                return res.json();
-            }
-            return res.text();
-        }).then((result) => {
-            // console.log(JSON.stringify(result));
-            if (typeof result === 'string') {
-                throw new Error(result);
-            }
-            const { code, msg, data: { list = undefined } = {} } = result;
-            if (code === 401) {
-                //todo 跳转到登录页
-                return;
-            }
-            resolve(list || result);
-        }).catch((error) => {
-            reject(error);
-        })
-    })
+  return new Promise((resolve, reject) => {
+    doAction
+      .then(res => {
+        //解析Content-Type 防止将非json数据进行json转换
+        const type = res.headers.get('Content-Type');
+        if ((type || '').indexOf('json') !== -1) {
+          return res.json();
+        }
+        return res.text();
+      })
+      .then(result => {
+        // console.log(JSON.stringify(result));
+        if (typeof result === 'string') {
+          throw new Error(result);
+        }
+        const {code, msg, data: {list = undefined} = {}} = result;
+        if (code === 401) {
+          //todo 跳转到登录页
+          return;
+        }
+        resolve(list || result);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 /**
  * 构建url参数
- * @param url 
- * @param params 
- * @returns 
+ * @param url
+ * @param params
+ * @returns
  */
 function buildParams(url: string, params?: {} | string): string {
-    let newUrl = new URL(url), finalUrl;
-    if (typeof params === 'object') {
-        for (const [key, value] of Object.entries(params)) {
-            newUrl.searchParams.append(key, value as string);
-        }
-        finalUrl = newUrl.toString();
-    } else if (typeof params === 'string') {
-        //适配path参数
-        finalUrl = url.endsWith("/") ? url + params : url + "/" + params;
-    } else {
-        finalUrl = newUrl.toString();
+  let newUrl = new URL(url),
+    finalUrl;
+  if (typeof params === 'object') {
+    for (const [key, value] of Object.entries(params)) {
+      newUrl.searchParams.append(key, value as string);
     }
-    console.log('---buildParams----:', finalUrl);
-    return finalUrl;
+    finalUrl = newUrl.toString();
+  } else if (typeof params === 'string') {
+    //适配path参数
+    finalUrl = url.endsWith('/') ? url + params : url + '/' + params;
+  } else {
+    finalUrl = newUrl.toString();
+  }
+  console.log('---buildParams----:', finalUrl);
+  return finalUrl;
 }
